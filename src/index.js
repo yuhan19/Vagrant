7a722534725c3ab576724613621d0e188f9687de
yuhan19 <yuhan.private@gmail.com>
2019-05-03 20:00:41 -0600

    configure webpack react babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('ook');
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const myDiv = <div>this is a div</div>;
+
+
+ReactDOM.render(myDiv, document.getElementById('app'));
